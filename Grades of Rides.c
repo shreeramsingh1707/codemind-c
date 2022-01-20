9a1539af14a3c7f45352ea444fe429c6dcb4c199
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-01-20 21:53:16 +0530

    #include<stdio.h> int main() {     int h,s,v;     scanf("%d%d%d",&h,&s,&v);     if(h>50 &&s>60 && v>100)     printf("10");     else if(h>50 && s>60 && v<=100)     printf("9");     else if(h<=50 && s>60 && v>100)     printf("8");     else if(h>50 && s<=60 && v>100)     printf("7");     else if(h>50 && s<=60 && v<=100 || h<=50 && s>60 && v<=100)     printf("6");     else     printf("5");     return 0;
    }

---
+++ Grades of Rides.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int h,s,v;
+    scanf("%d%d%d",&h,&s,&v);
+    if(h>50 &&s>60 && v>100)
+    printf("10");
+    else if(h>50 && s>60 && v<=100)
+    printf("9");
+    else if(h<=50 && s>60 && v>100)
+    printf("8");
+    else if(h>50 && s<=60 && v>100)
+    printf("7");
+    else if(h>50 && s<=60 && v<=100 || h<=50 && s>60 && v<=100)
+    printf("6");
+    else
+    printf("5");
+    return 0;
+
+    
+}
