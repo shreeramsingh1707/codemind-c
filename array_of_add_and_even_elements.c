a3c3b20e665153372d15d0d329e205f450f73004
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 00:44:21 +0530

    #include<stdio.h> int main() {     int i,n,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==1)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     return 0; }

--- array_of_add_and_even_elements.c
+++ array_of_add_and_even_elements.c
@@ -1,30 +1,25 @@
 #include<stdio.h>
-int main(){
-    int n,i,c=0;
+int main()
+{
+    int i,n,arr[100];
     scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    int b[n];
-    int j=0;
     for(i=0;i<n;i++)
     {
-       if(a[i]%2!=0){
-           b[j]=a[i];
-           j++;
-       }
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-       if(a[i]%2==0){
-           b[j]=a[i];
-           j++;
-       }
+        if(arr[i]%2==1)
+        {
+            printf("%d ",arr[i]);
+        }
     }
     for(i=0;i<n;i++)
     {
-        printf("%d ",b[i]);
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
     }
-    
     return 0;
 }
