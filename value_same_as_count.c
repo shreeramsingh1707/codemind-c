b044bd2678456a7be25ee20735862f6e759da71f
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 00:39:51 +0530

    #include<stdio.h> int main() {     int i,j,n,arr[100],c=0,k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==arr[i])         {             k++;             arr[i]=0;         }     }
            printf("%d",k);
        
    }

---
+++ value_same_as_count.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,arr[100],c=0,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==arr[i])
+        {
+            k++;
+            arr[i]=0;
+        }
+    }
+    
+    
+        printf("%d",k);
+    
+}
