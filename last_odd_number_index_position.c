6450729d49253080fbeb3bef9ac91373bbb61c34
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 00:58:19 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==1)         {             printf("%d",i);             break;         }     } }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==1)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
