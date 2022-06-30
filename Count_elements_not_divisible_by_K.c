099fef88a1b6dfc5ca2718a80671df74046be985
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 00:17:48 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],k,c=0;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],k,c=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
