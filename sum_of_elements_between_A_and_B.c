c578ec421e1388f32846955d8783926c061505b3
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 00:33:03 +0530

    #include<stdio.h> int main() {     int i,j,arr[100],n,a,b,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             s=s+arr[i];         }
        }
        printf("%d",s);
        return 0;
    }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,arr[100],n,a,b,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            s=s+arr[i];
+        }
+
+    }
+    printf("%d",s);
+    return 0;
+}
