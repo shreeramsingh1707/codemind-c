8ca54a69cf6d20b8468a46515106fd7013e1d8d5
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 11:13:22 +0530

    int pal(int n) {     int s=0,temp,d;     temp=n;     while(n!=0)     {         d=n%10;         s=s*10+d;         n=n/10;     }     if(s==temp)     return 1;     else     return 0; } int prime(int n) {     int i,c=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c=c+1;         }     }     if(c==2)     return 1;     else     return 0; } #include<stdio.h> #include<math.h> int main() {     int n,a,k;     scanf("%d",&n);     a=n+1;     while(1)     {         if(prime(a) && pal(a))         {             k=a;             break;         }         a=a+1;     }     printf("%d",k);
    }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,36 +1,49 @@
+int pal(int n)
+{
+    int s=0,temp,d;
+    temp=n;
+    while(n!=0)
+    {
+        d=n%10;
+        s=s*10+d;
+        n=n/10;
+    }
+    if(s==temp)
+    return 1;
+    else
+    return 0;
+}
+int prime(int n)
+{
+    int i,c=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c=c+1;
+        }
+    }
+    if(c==2)
+    return 1;
+    else
+    return 0;
+}
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,i,rev=0,r,m,temp,fact=0;
+    int n,a,k;
     scanf("%d",&n);
-    i=n+1;
-    while(i>0)
+    a=n+1;
+    while(1)
     {
-        rev=0;
-        temp=i;
-        fact=0;
-        while(temp>0)
+        if(prime(a) && pal(a))
         {
-            r=temp%10;
-            rev=(rev*10)+r;
-            temp/=10;
+            k=a;
+            break;
         }
-        if(i==rev)
-        {
-            for(m=1;m<=i;m++)
-            {
-                if(i%m==0)
-                {
-                    fact++;
-                }
-            }
-            if(fact==2)
-            {
-                printf("%d",i);
-                break;
-            }
-        }
-        i++;
+        a=a+1;
     }
-    return 0;
+    printf("%d",k);
+    
 }
