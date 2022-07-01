ef4ee4db6f4456214e64d0435fbd061160aba5e7
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 10:56:37 +0530

    #include<stdio.h> int main() {     int a[100],r,n,i,j,k=0,c1=0,c,f;     scanf("%d",&n);     while(n>0)     {         r=n%10;         a[k]=r;         k++;         n=n/10;     }     for(i=0;i<k;i++)     {         c=0;         for(j=0;j<k;j++)         {             if(a[i]==a[j] && i!=j)             {                 c=1;                 break;             }         }         if(c==0)         {             c1++;         }     }     if(c1==k)     printf("Unique Number");     else     printf("Not Unique Number"); }

--- Unique_number.c
+++ Unique_number.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
-
-int main(){
-    int n, i, digit;
-    static int arr[10];
-    scanf("%d", &n);
-    while(n>0){
-        digit = n%10;
-        arr[digit]++;
+#include<stdio.h>
+int main()
+{
+    int a[100],r,n,i,j,k=0,c1=0,c,f;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        a[k]=r;
+        k++;
         n=n/10;
     }
-    for(i=0; i<10; i++){
-        if(arr[i]>1){
-            printf("Not Unique Number");
-            break;
+    for(i=0;i<k;i++)
+    {
+        c=0;
+        for(j=0;j<k;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+            {
+                c=1;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            c1++;
         }
     }
-    if(i==10){
-        printf("Unique Number");
-    }
-    return 0;
-  
+    if(c1==k)
+    printf("Unique Number");
+    else
+    printf("Not Unique Number");
 }
